3d4853c8051390c90dd0dc6257be74e3d32c2690
Vinayak Kaushik <vinayakkaushikk@gmail.com>
2019-02-05 18:22:06 +0530

    Basic UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bulma'
+import Radium from 'radium'
+import Img from './sharing_options.png'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <body>
+        <div className="hero is-dark">
+          <div className="hero-body">
+            <div className="columns is-centered is-mobile">
+              <h1 style={styles.title} className="title">
+                XKCD
+              </h1>
+            </div>
+            <div className="columns is-centered is-mobile">
+              <h2 style={styles.subtitle} className="subtitle has-text-centered">
+                A webcomic of romance, sarcasm, math, and language.
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className="section">
+          <div className="columns is-centered is-mobile">
+            <div className="column is-narrow">
+            <h1 className="has-text-centered" style={styles.comicHeading}>SHARING OPTIONS</h1>
+            </div>
+          </div>
+        </div>
+        <div className="columns is-centered is-mobile">
+          <div className="column is-narrow">
+            <img src={Img}/>
+          </div>
+        </div>
+        <div className="section">
+          <div className="columns is-centered is-mobile">
+            <div className="level is-mobile">
+              <div className="level-item">
+                <a className="button" style={styles.navButton}>&lt;&lt;</a>
+              </div>
+              <div className="level-item">
+                <a className="button" style={styles.navButton}>&lt; PREV</a>
+              </div>
+              <div className="level-item">
+                <a className="button" style={styles.navButton}>RANDOM</a>
+              </div>
+                <div className="level-item">
+              <a className="button" style={styles.navButton}>NEXT &gt;</a>
+              </div>
+                <div className="level-item">
+                <a className="button" style={styles.navButton}>&gt;&gt;</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
     );
   }
+}
+
+App = Radium(App)
+
+const styles = {
+  navButton: {
+    fontFamily: "XKCD Script",
+  },
+  title: {
+    fontFamily: "XKCD Script",
+    fontSize: "50px",
+  },
+  subtitle: {
+    fontFamily: "XKCD Script",
+    fontSize: "20px",
+  },
+  comicHeading: {
+    fontFamily: "Arvo",
+    fontSize: "20px",
+  },
 }
 
 export default App;
